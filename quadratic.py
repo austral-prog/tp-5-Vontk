df4e830b378b0ce6cb0789a25f38e1dbf0ca2408
Vontk <121203087+Vontk@users.noreply.github.com>
2024-04-26 09:47:08 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,19 @@
-# Replace the "ANSWER HERE" for your answer
-
 def roots(a, b, c):
-    return "ANSWER HERE"
+    sqrt = (b**2 - 4*a*c)**0.5
+    r1 = (-b + sqrt)/(2*a)
+    r2 = (-b - sqrt)/(2*a)
+    return f"{r1}, {r2}"
 
-
-def value_y(a, b, c, x):
-    return "ANSWER HERE"
-
+def value_y(a,b,c,x):
+    y=a*x**2+b*x+c
+    return y
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
-
+    if b > 0:
+        b=f"+ {b}x"
+    if c > 0:
+        c =f"+ {c}"
+    return f"f(x)= {a}x {b} {c}"
 
 def derivation(a, b):
-    return "ANSWER HERE"
+    return f"2{a}x + {b}"

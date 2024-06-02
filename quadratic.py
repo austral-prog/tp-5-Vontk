72649f63c684cc0e6c2debaef9ef75a919b2a79b
Matias P. Menendez <121203087+Vontk@users.noreply.github.com>
2024-06-01 22:12:23 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -2,18 +2,36 @@ def roots(a, b, c):
     sqrt = (b**2 - 4*a*c)**0.5
     r1 = (-b + sqrt)/(2*a)
     r2 = (-b - sqrt)/(2*a)
-    return f"{r1}, {r2}"
+    if "j" in str(r1) or "j" in str(r2):
+        return "( )"
+    if r1 == r2:
+        return f'({r1})'
+    else:
+        return f"({r1}, {r2})"
 
-def value_y(a,b,c,x):
-    y=a*x**2+b*x+c
+
+def value_y(a, b, c, x):
+    y = a * x ** 2 + b * x + c
     return y
 
-def to_string(a, b, c):
-    if b > 0:
-        b=f"+ {b}x"
-    if c > 0:
-        c =f"+ {c}"
-    return f"f(x)= {a}x {b} {c}"
 
-def derivation(a, b):
-    return f"2{a}x + {b}"
+def to_string(a, b, c):
+    if a != 0 and b == 0:
+        return f"f(x) = {a} * X^2 + {c}"
+    if a == 0 and b != 0:
+        return f"f(x) = {b} * X + {c}"
+    if a == 0 and b == 0:
+        return f"f(x) = {c}"
+    return f"f(x) = {a} * X^2 + {b} * X + {c}"
+print(to_string(0, 0, 5) ) # Retorna: "f(x) = 5"
+
+
+def derivation(a, b, c):
+    if a != 0 and b != 0:
+        return f"f'(x) = {2*a} * X + {b}"
+    elif a != 0 and b == 0:
+        return f"f'(x) = {2*a} * X"
+    elif a == 0 and b != 0:
+        return f"f'(x) = {b}"
+    else:
+        return f"f'(x) = 0"
